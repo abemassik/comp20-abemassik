ec8e1515d4b41a06eb7e36026fee81e2767b09e0
Abe Massik <abe.massik@gmail.com>
2017-06-14 12:15:50 -0700

    Map and initial marker showing
    Zoom issue, works properly when opening, not when running through SimpleHTTPServer

--- mbta/tMap.js
+++ mbta/tMap.js
@@ -1,19 +1,29 @@
 // Map initialization JavaScript
 
 var southStation = new google.maps.LatLng(42.352271, -71.05524200000001);
+var andrew = new google.maps.LatLng(42.330154, -71.057655);
+var porter = new google.maps.LatLng(42.3884, -71.11914899999999);
+var harvard = new google.maps.LatLng(42.373362, -71.118956);
+
 var options = {
         center: southStation,
-        zoom: 5,
+        zoom: 12,
         mapTypeId: google.maps.MapTypeId.ROADMAP
 }
 
-function createMap() {
-        var map = new google.maps.Map(document.getElementById('map'), options);
+function createMap() {         var map = new
+google.maps.Map(document.getElementById('map'), options);
 
-        var marker = new google.maps.Marker({
+        var southStationM = new google.maps.Marker({
                 position:southStation,
-                title: "test"
+                title: "South Station"
         });
-        marker.setMap(map);
+        southStationM.setMap(map);
+
+        var andrewM = new google.maps.Marker({
+                position: andrew,
+                title: Andrew
+        });
+        andrewM.setMap(map);
 }
 
