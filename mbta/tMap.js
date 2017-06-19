ecf4181de93768a600e7234b73d4b94e0453d246
Abe Massik <abe.massik@gmail.com>
2017-06-18 18:23:37 -0700

    working location marker, figuring out shortest distance

--- mbta/tMap.js
+++ mbta/tMap.js
@@ -1,35 +1,55 @@
-// Map initialization JavaScript
+// MBTA Lab JavaScript
+southStation = new google.maps.LatLng(42.352271, -71.05524200000001);
+andrew = new google.maps.LatLng(42.330154, -71.057655);
+porter = new google.maps.LatLng(42.3884, -71.11914899999999);
+harvard = new google.maps.LatLng(42.373362, -71.118956);
+umass = new google.maps.LatLng(42.320685, -71.052391);
+savin = new google.maps.LatLng(42.31129, -71.053331);
+parkSt = new google.maps.LatLng(42.35639457, -71.0624242);
+broadway = new google.maps.LatLng(42.342622, -71.056967);
+northQuincy = new google.maps.LatLng(42.275275, -71.029583);
+shawmut = new google.maps.LatLng(42.29312583, -71.06573796000001);
+davis = new google.maps.LatLng(42.39674, -71.121815);
+alewife = new google.maps.LatLng(42.395428, -71.142483);
+kendall = new google.maps.LatLng(42.36249079, -71.08617653);
+charlesMGH = new google.maps.LatLng(42.361166, -71.070628);
+dtnCrs = new google.maps.LatLng(42.355518, -71.060225);
+quincyCtr = new google.maps.LatLng(42.251809, -71.005409);
+quincyAdams = new google.maps.LatLng(42.233391, -71.007153);
+ashmont = new google.maps.LatLng(42.284652, -71.06448899999999);
+wollaston = new google.maps.LatLng(42.2665139, -71.0203369);
+fields = new google.maps.LatLng(42.300093, -71.061667);
+central = new google.maps.LatLng(42.365486, -71.103802);
+braintree = new google.maps.LatLng(42.2078543, -71.0011385);
 
-var southStation = new google.maps.LatLng(42.352271, -71.05524200000001);
-var andrew = new google.maps.LatLng(42.330154, -71.057655);
-var porter = new google.maps.LatLng(42.3884, -71.11914899999999);
-var harvard = new google.maps.LatLng(42.373362, -71.118956);
-var umass = new google.maps.LatLng(42.320685, -71.052391);
-var savin = new google.maps.LatLng(42.31129, -71.053331);
-var parkSt = new google.maps.LatLng(42.35639457, -71.0624242);
-var broadway = new google.maps.LatLng(42.342622, -71.056967);
-var northQuincy = new google.maps.LatLng(42.275275, -71.029583);
-var shawmut = new google.maps.LatLng(42.29312583, -71.06573796000001);
-var davis = new google.maps.LatLng(42.39674, -71.121815);
-var alewife = new google.maps.LatLng(42.395428, -71.142483);
-var kendall = new google.maps.LatLng(42.36249079, -71.08617653);
-var charlesMGH = new google.maps.LatLng(42.361166, -71.070628);
-var dtnCrs = new google.maps.LatLng(42.355518, -71.060225);
-var quincyCtr = new google.maps.LatLng(42.251809, -71.005409);
-var quincyAdams = new google.maps.LatLng(42.233391, -71.007153);
-var ashmont = new google.maps.LatLng(42.284652, -71.06448899999999);
-var wollaston = new google.maps.LatLng(42.2665139, -71.0203369);
-var fields = new google.maps.LatLng(42.300093, -71.061667);
-var central = new google.maps.LatLng(42.365486, -71.103802);
-var braintree = new google.maps.LatLng(42.2078543, -71.0011385);
+redLineStops = [{name: "South Station", location: southStation},
+                {name: "Andrew", location: andrew},
+                {name: "Porter Square", location: porter},
+                {name: "Harvard Square", location: harvard},
+                {name: "JFK/UMass", location: umass},
+                {name: "Savin Hill", location: savin},
+                {name: "Park Street", location: parkSt},
+                {name: "Broadway", location: broadway},
+                {name: "North Quincy", location: northQuincy},
+                {name: "Shawmut", location: shawmut},
+                {name: "Davis Square", location: davis},
+                {name: "Alewife", location: alewife},
+                {name: "Kendall Square/MIT", location: kendall},
+                {name: "Charles/MGH", location: charlesMGH},
+                {name: "Downtown Crossing", location: dtnCrs},
+                {name: "Quincy Center", location: quincyCtr},
+                {name: "Quincy Adams", location: quincyAdams},
+                {name: "Ashmont", location: ashmont},
+                {name: "Wollaston", location: wollaston},
+                {name: "Fields Corner", location: fields},
+                {name: "Central Square", location: central},
+                {name: "Braintree", location: braintree}];
 
-var route = [alewife, davis, porter, harvard, central, kendall, charlesMGH,
+route = [alewife, davis, porter, harvard, central, kendall, charlesMGH,
              parkSt, dtnCrs, southStation, broadway, andrew, umass,
              northQuincy, wollaston, quincyCtr, quincyAdams, braintree];
 
-var branch = [umass, savin, fields, shawmut, ashmont];
-
-
+branch = [umass, savin, fields, shawmut, ashmont];
 
 var options = {
         center: southStation,
@@ -39,33 +59,17 @@ var options = {
 
 
 function createMap() {         
-        var map = new google.maps.Map(document.getElementById('map'), options);
+        map = new google.maps.Map(document.getElementById('map'), options);
+
+        makeRedLine();
+
+        addGeoLoc();
 
         
+}
 
-
-        addStop (southStation, "South Station");
-        addStop (andrew, "Andrew");
-        addStop (porter, "Porter Square");
-        addStop (harvard, "Harvard Square");
-        addStop (umass, "JFK/UMass");
-        addStop (savin, "Savin Hill");
-        addStop (parkSt, "Park Street");
-        addStop (broadway, "Broadway");
-        addStop (northQuincy, "North Quincy");
-        addStop (shawmut, "Shawmut");
-        addStop (davis, "Davis Square");
-        addStop (alewife, "Alewife");
-        addStop (kendall, "Kendall Square/MIT");
-        addStop (charlesMGH, "Charles/MGH");
-        addStop (dtnCrs, "Downtown Crossing");
-        addStop (quincyCtr, "Quincy Center");
-        addStop (quincyAdams, "Quincy Adams");
-        addStop (ashmont, "Ashmont");
-        addStop (wollaston, "Wollaston");
-        addStop (fields, "Fields Corner");
-        addStop (central, "Central Square");
-        addStop (braintree, "Braintree");
+function makeRedLine() {
+        redLineStops.forEach(addStop);
 
         var redLine = new google.maps.Polyline ({
                 path: route,
@@ -80,24 +84,62 @@ function createMap() {
                 strokeWeight: 2,
                 map: map
         });
-
-
-        
-        
-
-        function addStop (coord, name) {
-                var marker = new google.maps.Marker({
-                        position:coord,
-                        title: name,
-                        map: map,
-                        icon: 'favicon.ico'
-                });
-                var info = new google.maps.InfoWindow({
-                        content: name
-                });
-                marker.addListener('click', function() {
-                        info.open(map,marker);
-                })
-        }
 }
+
+function addStop (element, index, array) {
+        var marker = new google.maps.Marker({
+                position:element.location,
+                title: element.name,
+                map: map,
+                icon: 'favicon.ico'
+        });
+        var info = new google.maps.InfoWindow({
+                content: element.name
+        });
+        marker.addListener('click', function() {
+                info.open(map,marker);
+        });
+}
+
+function addGeoLoc () {
+        navigator.geolocation.getCurrentPosition(findPosition);
+        
+}
+
+function findClosest () {
+        closestStop = redLineStops[0];
+        closestDistance = google.maps.geometry.spherical.computeDistanceBetween(
+                myPosition, closestStop.position);
+
+        for (var i = 0; i < redLineStops.length; i++) {
+                var currentDistance = 
+                google.maps.geometry.spherical.computeDistanceBetween(
+                        myPosition, redLineStops[i].position);
+                if (currentDistance < closestDistance) {
+                        closestStop = redLineStops[i];
+                        closestDistance = currentDistance;
+                }
+        }
+
+}
+
+function findPosition (position) {
+        myPosition =  new google.maps.LatLng(position.coords.latitude, 
+                position.coords.longitude);
+
+        var marker = new google.maps.Marker({
+                position: myPosition,
+                title: "Your Location",
+                map: map,
+        });
+        var info = new google.maps.InfoWindow({
+                content: "The closest stop is " + closestStop.name
+        });
+        marker.addListener('click', function() {
+                info.open(map,marker);
+        });
+}
+
+
+
 
