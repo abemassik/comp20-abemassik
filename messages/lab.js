0851a6448a27ed2301203650579cfd18e22c3df8
Abe Massik <abe.massik@gmail.com>
2017-06-21 10:55:23 +0100

    Finished step 1 of messages lab part 2

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,29 @@
 // Your JavaScript goes here...
+
+function parse() {
+        call = new XMLHttpRequest();
+
+        call.open('GET', 'data.json', true);
+
+        
+        call.onreadystatechange = function() {
+                if (call.readyState == 4 && call.status == 200) {
+                        messageInfo = JSON.parse(call.responseText);
+
+                        output = "";
+
+                        for (var i = 0; i < messageInfo.length; i++) {
+                                output += "<p id=\"bubble\">" + 
+                                messageInfo[i].username + ": " + 
+                                messageInfo[i].content + "</p>";
+                        }
+                        document.getElementById('messages').innerHTML = output;
+                }
+        }
+
+        call.send();
+
+
+
+
+}
