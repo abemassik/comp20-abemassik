f71a9e59ac6aef73b37bfde82d8ffda3047612bc
Abe Massik <abe.massik@gmail.com>
2017-06-21 23:31:24 +0100

    arrivals found, not added to infomessage

--- mbta/tMap.js
+++ mbta/tMap.js
@@ -23,160 +23,173 @@ central = new google.maps.LatLng(42.365486, -71.103802);
 braintree = new google.maps.LatLng(42.2078543, -71.0011385);
 
 redLineStops = [{name: "South Station", location: southStation},
-                {name: "Andrew", location: andrew},
-                {name: "Porter Square", location: porter},
-                {name: "Harvard Square", location: harvard},
-                {name: "JFK/UMass", location: umass},
-                {name: "Savin Hill", location: savin},
-                {name: "Park Street", location: parkSt},
-                {name: "Broadway", location: broadway},
-                {name: "North Quincy", location: northQuincy},
-                {name: "Shawmut", location: shawmut},
-                {name: "Davis Square", location: davis},
-                {name: "Alewife", location: alewife},
-                {name: "Kendall Square/MIT", location: kendall},
-                {name: "Charles/MGH", location: charlesMGH},
-                {name: "Downtown Crossing", location: dtnCrs},
-                {name: "Quincy Center", location: quincyCtr},
-                {name: "Quincy Adams", location: quincyAdams},
-                {name: "Ashmont", location: ashmont},
-                {name: "Wollaston", location: wollaston},
-                {name: "Fields Corner", location: fields},
-                {name: "Central Square", location: central},
-                {name: "Braintree", location: braintree}];
+{name: "Andrew", location: andrew},
+{name: "Porter Square", location: porter},
+{name: "Harvard Square", location: harvard},
+{name: "JFK/UMass", location: umass},
+{name: "Savin Hill", location: savin},
+{name: "Park Street", location: parkSt},
+{name: "Broadway", location: broadway},
+{name: "North Quincy", location: northQuincy},
+{name: "Shawmut", location: shawmut},
+{name: "Davis Square", location: davis},
+{name: "Alewife", location: alewife},
+{name: "Kendall Square/MIT", location: kendall},
+{name: "Charles/MGH", location: charlesMGH},
+{name: "Downtown Crossing", location: dtnCrs},
+{name: "Quincy Center", location: quincyCtr},
+{name: "Quincy Adams", location: quincyAdams},
+{name: "Ashmont", location: ashmont},
+{name: "Wollaston", location: wollaston},
+{name: "Fields Corner", location: fields},
+{name: "Central Square", location: central},
+{name: "Braintree", location: braintree}];
 
 route = [alewife, davis, porter, harvard, central, kendall, charlesMGH,
-             parkSt, dtnCrs, southStation, broadway, andrew, umass,
-             northQuincy, wollaston, quincyCtr, quincyAdams, braintree];
+parkSt, dtnCrs, southStation, broadway, andrew, umass,
+northQuincy, wollaston, quincyCtr, quincyAdams, braintree];
 
 branch = [umass, savin, fields, shawmut, ashmont];
 
 var options = {
-        center: southStation,
-        zoom: 12,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
+    center: southStation,
+    zoom: 12,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
 }
 
 
 function createMap() {         
-        map = new google.maps.Map(document.getElementById('map'), options);
+    map = new google.maps.Map(document.getElementById('map'), options);
 
-        getArrivals();
 
-        makeRedLine();
+    makeRedLine();
 
-        addGeoLoc();
+    addGeoLoc();
 
-        
+
 }
 
 function makeRedLine() {
-        redLineStops.forEach(addStop);
+    redLineStops.forEach(addStop);
 
-        var redLine = new google.maps.Polyline ({
-                path: route,
-                strokeColor: "#ef1a2c",
-                strokeWeight: 2,
-                map: map
-        });
+    var redLine = new google.maps.Polyline ({
+        path: route,
+        strokeColor: "#ef1a2c",
+        strokeWeight: 2,
+        map: map
+    });
 
-        var redLineBranch = new google.maps.Polyline ({
-                path: branch,
-                strokeColor: "#ef1a2c",
-                strokeWeight: 2,
-                map: map
-        });
+    var redLineBranch = new google.maps.Polyline ({
+        path: branch,
+        strokeColor: "#ef1a2c",
+        strokeWeight: 2,
+        map: map
+    });
 }
 
 function addStop (element, index, array) {
-        var marker = new google.maps.Marker({
-                position:element.location,
-                title: element.name,
-                map: map,
-                icon: 'favicon.ico'
-        });
-        var info = new google.maps.InfoWindow({
-                content: ""
-        });
-        marker.addListener('click', function() {
-                info.open(map,marker);
-        });
+    var marker = new google.maps.Marker({
+        position:element.location,
+        title: element.name,
+        map: map,
+        icon: 'favicon.ico'
+    });
+    var info = new google.maps.InfoWindow({
+        content: ""
+    });
+    marker.addListener('click', function() {
+        var message = getArrivals(element.name);
+        info.setContent(message);
+        info.open(map,marker);
+    });
 }
 
 function addGeoLoc () {
-        navigator.geolocation.getCurrentPosition(findPosition);   
+    navigator.geolocation.getCurrentPosition(findPosition);   
 }
 
 
 function findPosition (position) {
-        myPosition =  new google.maps.LatLng(position.coords.latitude, 
-                position.coords.longitude);
+    myPosition =  new google.maps.LatLng(position.coords.latitude, 
+        position.coords.longitude);
 
-        closestStop = redLineStops[0];
-        shortestDistance = 
-                google.maps.geometry.spherical.computeDistanceBetween(
-                        myPosition, redLineStops[0].location);
-        for (var i = 0; i < redLineStops.length; i++) {
-                currentDistance = 
-                        google.maps.geometry.spherical.computeDistanceBetween(
-                                myPosition, redLineStops[i].location);
-                if (currentDistance < shortestDistance) {
-                        shortestDistance = currentDistance;
-                        closestStop = redLineStops[i];
-                }
+    closestStop = redLineStops[0];
+    shortestDistance = 
+    google.maps.geometry.spherical.computeDistanceBetween(
+        myPosition, redLineStops[0].location);
+    for (var i = 0; i < redLineStops.length; i++) {
+        currentDistance = 
+        google.maps.geometry.spherical.computeDistanceBetween(
+            myPosition, redLineStops[i].location);
+        if (currentDistance < shortestDistance) {
+            shortestDistance = currentDistance;
+            closestStop = redLineStops[i];
         }
+    }
 
-        var marker = new google.maps.Marker({
-                position: myPosition,
-                title: "Your Location",
-                map: map,
-        });
-        var info = new google.maps.InfoWindow({
-                content: "The closest stop is " + closestStop.name
-        });
-        marker.addListener('click', function() {
-                info.open(map,marker);
-        });
+    var marker = new google.maps.Marker({
+        position: myPosition,
+        title: "Your Location",
+        map: map,
+    });
+    var info = new google.maps.InfoWindow({
+        content: "The closest stop is " + closestStop.name
+    });
+    marker.addListener('click', function() {
+        info.open(map,marker);
+    });
 
-        var path = [myPosition, closestStop.location];
+    var path = [myPosition, closestStop.location];
 
-        var poly = new google.maps.Polyline ({
-                path: path,
-                strokeWeight: 1,
-                strokeColor: "#190000",
-                map: map
-        });
+    var poly = new google.maps.Polyline ({
+        path: path,
+        strokeWeight: 1,
+        strokeColor: "#190000",
+        map: map
+    });
 }
 
 function getArrivals (stop) {
-        infoMessage = "Trains arriving in: ";
+    infoMessage = "Trains arriving in: ";
 
-        request = new XMLHttpRequest();
+    request = new XMLHttpRequest();
 
-        request.open('GET', 'https://defense-in-derpth.herokuapp.com/redline.json', true);
+    request.open('GET', 'https://defense-in-derpth.herokuapp.com/redline.json', true);
 
-        
-        request.onreadystatechange = function(stop) {
-                if (request.readyState == 4 && request.status == 200) {
-                        tripInfo = JSON.parse(request.responseText);
+    request.onreadystatechange = function() {
+        if (request.readyState == 4 && request.status == 200) {
+            tripInfo = JSON.parse(request.responseText);
 
-                        
+            for (var trips = tripInfo.TripList.Trips.length - 1; 
+               trips >= 0; trips--) {
+                for (var predictions = 
+                   tripInfo.TripList.Trips[trips].Predictions.length - 1; 
+                   predictions >= 0; predictions--) {
+                if (tripInfo.TripList.Trips[trips].Predictions[predictions].Stop
+                    == stop) {
+                var seconds = tripInfo.TripList.Trips[trips].Predictions[predictions].Seconds;
+                var timeToArrival = convert(seconds);
+
+                infoMessage += timeToArrival.toString() + "minutes ";
+
                 }
-                
+                }
+            }
         }
 
-        request.send();
+    }
 
-        return infoMessage;
+request.send();
+
+console.log(infoMessage);
 
 }
 
-function formatTime (rawTime) {
-        minutes = Math.floor(rawTime/60);
-        seconds = rawTime - minutes*60;
-
-        return minutes.toString() + ":" + seconds.toString();
+function convert(seconds) {
+    var minutes = Math.floor(seconds/60);
+    return minutes;
 }
+
 
 
 
